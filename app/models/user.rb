873787e97f6d16738d56415088ffe6894a9b2574
jmonto55 <jmonto55@gmail.com>
2023-05-29 22:03:09 -0500

    Add total spent method to calculate all amounts per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :records, foreign_key: :user_id
-  has_many :categories, foreign_key: :user_id
+  has_many :categories, foreign_key: :user_id, dependent: :destroy
+  
+  def total_spent
+    categories.sum { |category| category.expenses.sum(&:amount) } || 0
+  end  
 end
