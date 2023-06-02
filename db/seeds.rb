c5e360872e427afe0108985b7cc7b5fe311b3b0c
jmonto55 <jmonto55@gmail.com>
2023-06-02 10:15:11 -0500

    Apply final style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ u1.save
 # User 1 Categories
 
 Category.create(user_id: u1.id, name: 'Leisure', icon: 'https://img.icons8.com/2266EE/color/debt')
-Category.create(user_id: u1.id, name: 'Groceries', icon: 'https://img.icons8.com/2266EE/color/dog')
+Category.create(user_id: u1.id, name: 'Groceries', icon: 'https://img.icons8.com/2266EE/color/plastic-bag')
 Category.create(user_id: u1.id, name: 'Investment', icon: 'https://img.icons8.com/2266EE/color/sport')
-Category.create(user_id: u1.id, name: 'Rent', icon: 'https://img.icons8.com/2266EE/color/bank')
+Category.create(user_id: u1.id, name: 'Pet', icon: 'https://img.icons8.com/2266EE/color/dog')
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Test Account', email: 'test@mail.com', password: 'password') }
+  let(:food) do
+    Category.create(user_id: u1.id, name: 'Leisure', icon: 'https://img.icons8.com/2266EE/color/debt')
+  end
+
+  describe 'attributes' do
+    it 'has a name' do
+      expect(food.name).to eq('Beans')
+    end
+
+    it 'has "grams" as measurement unit' do
+      expect(food.measurement_unit).to eq('grams')
+    end
+
+    it 'has a price of 10' do
+      expect(food.price).to eq(10.0)
+    end
+  end
 end
